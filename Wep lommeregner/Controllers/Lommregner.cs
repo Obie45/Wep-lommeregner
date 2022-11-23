3befac9168cbc763cd38740017f2897487faee44
eSport 7 <eSport 7@DESKTOP-M3AH0AM>
2022-11-23 17:14:25 +0100

    Add project files.

---
+++ Wep lommeregner/Controllers/Lommregner.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using org.mariuszgromada.math.mxparser;
+
+namespace Wep_lommeregner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Lommregner : ControllerBase
+    {
+        [HttpPost(Name = "Calculate2")]
+        public string Post(string regnestykke)
+        {
+            Expression exp = new Expression(regnestykke);
+            var value = exp.calculate();
+            return value.ToString();
+        }
+    }
+}
